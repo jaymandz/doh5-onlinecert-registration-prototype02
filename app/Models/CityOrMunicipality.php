f9d48592bf29190d6b13c3d0ac0e1ea006e60992
Jvergel Mandane <jvergel.mandane@gmail.com>
2023-03-08 13:51:55 +0800

    Added migrations and models from previous iteration

---
+++ app/Models/CityOrMunicipality.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CityOrMunicipality extends Model
+{
+    use HasFactory;
+
+    protected $table = 'cities_municipalities';
+}
---
+++ database/migrations/2023_03_08_054942_create_barangays_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barangays', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('city_municipality_id')->constrained('cities_municipalities');
+            $table->string('psgc', 10);
+            $table->string('name', 512);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barangays');
+    }
+};
